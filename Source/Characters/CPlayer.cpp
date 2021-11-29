5b8df40c4cad8a5470bc026b37e7f7bba7743f22
KMC <48755251+sksqwer@users.noreply.github.com>
2021-11-29 21:38:40 +0900

    Initial commit

---
+++ Source/Characters/CPlayer.cpp
@@ -0,0 +1,26 @@
+#include "CPlayer.h"
+
+ACPlayer::ACPlayer()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ACPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/UnrealCppProject2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealCppProject2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealCppProject2, "UnrealCppProject2" );
